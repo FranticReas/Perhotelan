813203fc04f7fb1d957bca4e36b114b996eadb5f
Muhammad Muhammad <muhammad.em6@gmail.com>
2025-01-02 22:47:35 +0700

    form privacy policy

---
+++ Perhotelan/View/frmPrivacy.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Perhotelan.View
+{
+    partial class frmPrivacy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrivacy));
+            pnlHeader = new Panel();
+            lblHotel = new Label();
+            lblKu = new Label();
+            btnHome = new Button();
+            btnBooking = new Button();
+            btnProfil = new Button();
+            btnSearch = new Button();
+            pnlSideBar = new Panel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            lblPriv = new Label();
+            pnlHeader.SuspendLayout();
+            pnlSideBar.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            pnlHeader.BackColor = Color.FromArgb(17, 70, 60);
+            pnlHeader.Controls.Add(lblHotel);
+            pnlHeader.Controls.Add(lblKu);
+            pnlHeader.Dock = DockStyle.Top;
+            pnlHeader.Location = new Point(0, 0);
+            pnlHeader.Margin = new Padding(3, 4, 3, 4);
+            pnlHeader.Name = "pnlHeader";
+            pnlHeader.Size = new Size(445, 85);
+            pnlHeader.TabIndex = 9;
+            // 
+            // lblHotel
+            // 
+            lblHotel.AutoSize = true;
+            lblHotel.BackColor = Color.Transparent;
+            lblHotel.FlatStyle = FlatStyle.Flat;
+            lblHotel.Font = new Font("Montserrat Black", 32F, FontStyle.Bold);
+            lblHotel.ForeColor = SystemColors.ControlLightLight;
+            lblHotel.Location = new Point(66, 0);
+            lblHotel.Margin = new Padding(0);
+            lblHotel.Name = "lblHotel";
+            lblHotel.Size = new Size(224, 75);
+            lblHotel.TabIndex = 12;
+            lblHotel.Text = "HOTEL";
+            // 
+            // lblKu
+            // 
+            lblKu.AutoEllipsis = true;
+            lblKu.AutoSize = true;
+            lblKu.BackColor = Color.Transparent;
+            lblKu.Font = new Font("Montserrat Black", 32F, FontStyle.Bold);
+            lblKu.ForeColor = Color.FromArgb(250, 199, 131);
+            lblKu.Location = new Point(255, 0);
+            lblKu.Margin = new Padding(0);
+            lblKu.Name = "lblKu";
+            lblKu.Size = new Size(114, 75);
+            lblKu.TabIndex = 13;
+            lblKu.Text = "KU";
+            // 
+            // btnHome
+            // 
+            btnHome.FlatAppearance.BorderColor = Color.White;
+            btnHome.FlatAppearance.BorderSize = 0;
+            btnHome.FlatStyle = FlatStyle.Flat;
+            btnHome.Font = new Font("Segoe UI", 12F);
+            btnHome.ForeColor = Color.White;
+            btnHome.Location = new Point(14, 4);
+            btnHome.Margin = new Padding(3, 4, 3, 4);
+            btnHome.Name = "btnHome";
+            btnHome.Size = new Size(48, 53);
+            btnHome.TabIndex = 4;
+            btnHome.Text = "🏠";
+            btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnBooking
+            // 
+            btnBooking.FlatAppearance.BorderColor = Color.White;
+            btnBooking.FlatAppearance.BorderSize = 0;
+            btnBooking.FlatStyle = FlatStyle.Flat;
+            btnBooking.Font = new Font("Segoe UI", 12F);
+            btnBooking.ForeColor = SystemColors.ControlLightLight;
+            btnBooking.Location = new Point(123, 4);
+            btnBooking.Margin = new Padding(3, 4, 3, 4);
+            btnBooking.Name = "btnBooking";
+            btnBooking.Size = new Size(48, 53);
+            btnBooking.TabIndex = 3;
+            btnBooking.Text = "📖";
+            btnBooking.UseVisualStyleBackColor = true;
+            // 
+            // btnProfil
+            // 
+            btnProfil.FlatAppearance.BorderSize = 0;
+            btnProfil.FlatStyle = FlatStyle.Flat;
+            btnProfil.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnProfil.ForeColor = Color.FromArgb(250, 199, 131);
+            btnProfil.Location = new Point(383, 4);
+            btnProfil.Margin = new Padding(3, 4, 3, 4);
+            btnProfil.Name = "btnProfil";
+            btnProfil.Size = new Size(48, 53);
+            btnProfil.TabIndex = 0;
+            btnProfil.Text = "🙍";
+            btnProfil.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            btnSearch.FlatAppearance.BorderColor = Color.White;
+            btnSearch.FlatAppearance.BorderSize = 0;
+            btnSearch.FlatStyle = FlatStyle.Flat;
+            btnSearch.Font = new Font("Segoe UI", 12F);
+            btnSearch.ForeColor = SystemColors.ControlLightLight;
+            btnSearch.Location = new Point(261, 4);
+            btnSearch.Margin = new Padding(3, 4, 3, 4);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(48, 53);
+            btnSearch.TabIndex = 2;
+            btnSearch.Text = "🔍";
+            btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // pnlSideBar
+            // 
+            pnlSideBar.BackColor = Color.FromArgb(17, 70, 60);
+            pnlSideBar.Controls.Add(btnHome);
+            pnlSideBar.Controls.Add(btnBooking);
+            pnlSideBar.Controls.Add(btnProfil);
+            pnlSideBar.Controls.Add(btnSearch);
+            pnlSideBar.Dock = DockStyle.Bottom;
+            pnlSideBar.Location = new Point(0, 539);
+            pnlSideBar.Margin = new Padding(3, 0, 3, 4);
+            pnlSideBar.Name = "pnlSideBar";
+            pnlSideBar.Size = new Size(445, 61);
+            pnlSideBar.TabIndex = 10;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.Controls.Add(lblPriv);
+            flowLayoutPanel1.Location = new Point(0, 92);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(445, 444);
+            flowLayoutPanel1.TabIndex = 11;
+            // 
+            // lblPriv
+            // 
+            lblPriv.AutoSize = true;
+            lblPriv.Location = new Point(3, 0);
+            lblPriv.Name = "lblPriv";
+            lblPriv.Size = new Size(439, 6180);
+            lblPriv.TabIndex = 0;
+            lblPriv.Text = resources.GetString("lblPriv.Text");
+            // 
+            // frmPrivacy
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(445, 600);
+            ControlBox = false;
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(pnlSideBar);
+            Controls.Add(pnlHeader);
+            Name = "frmPrivacy";
+            Text = "HotelKu";
+            pnlHeader.ResumeLayout(false);
+            pnlHeader.PerformLayout();
+            pnlSideBar.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlHeader;
+        private Label lblHotel;
+        private Label lblKu;
+        private Button btnHome;
+        private Button btnBooking;
+        private Button btnProfil;
+        private Button btnSearch;
+        private Panel pnlSideBar;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label lblPriv;
+    }
+}
---
+++ Perhotelan/View/frmPrivacy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using Perhotelan.Model.Context;
+using Perhotelan.Model.Entity;
+using Perhotelan.Model.Repository;
+using Perhotelan.View;
+using Perhotelan.Controller;
+
+namespace Perhotelan.View
+{
+    public partial class frmPrivacy : Form
+    {
+        private int _userId;
+        public frmPrivacy(int userId)
+        {
+            InitializeComponent();
+            _userId = userId;
+        }
+    }
+}
